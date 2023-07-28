1164c86af270fb1dd898e3a48939bb6df1ee7fe3
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-28 23:15:39 +0100

    Our custom error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,10 +15,14 @@ def teardown(self):
     """Remove the current SQLAlchemy session"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """Create a handler for 404 errors"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
