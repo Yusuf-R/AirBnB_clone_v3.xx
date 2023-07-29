20ae1fd6d2802a64cf042dbf4ee0b29beae6eb7d
saria2016 <saria.m.mohammed@gmail.com>
2023-07-29 13:46:55 +0300

    fix task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,13 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def closedb(db_close):
     """Remove the current SQLAlchemy session"""
     storage.close()
 
@@ -24,5 +25,5 @@ def not_found(error):
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True)
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
